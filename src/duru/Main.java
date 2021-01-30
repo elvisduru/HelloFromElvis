1733360af50ed388a84af07417ff4ca8881e6461
carlosavalle <carlosavalle@users.noreply.github.com>
2021-01-30 17:04:33 -0300

    I added a new output

--- src/duru/Main.java
+++ src/duru/Main.java
@@ -3,6 +3,8 @@ package duru;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println( "Hello, my name is Elvis");
+        System.out.println("Hello Elvis, my name is Carlos");
     }
 }
